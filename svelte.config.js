733be1fe2c729ea116e106cec95cd066231ee4df
Jorge Fuerte <36144377+JorjoF@users.noreply.github.com>
2021-10-20 19:29:36 -0400

    updated for heroku

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import  vercel  from '@sveltejs/adapter-vercel';
+import  node  from '@sveltejs/adapter-node';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: node({ env: { port: process|.env.PORT } }),
 	}
 };
 
